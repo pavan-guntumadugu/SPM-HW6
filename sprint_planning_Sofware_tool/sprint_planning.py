a69d2ed934677fb9976879fcd5ab8209485cedd5
gpavan123789 <106580961+gpavan123789@users.noreply.github.com>
2024-03-03 22:53:22 -0500

    calculating effor hour capacity for team

--- sprint_planning_Sofware_tool/sprint_planning.py
+++ sprint_planning_Sofware_tool/sprint_planning.py
@@ -12,6 +12,12 @@ def calculate_effort_hour_capacity_per_person(num_sprint_days, team_member_detai
         total_hours_available_per_person += member_detail['hours_per_day'] * (num_sprint_days - sum(member_detail['time_off']))
     return total_hours_available_per_person
 
+# effort_capacity.py
+def calculate_effort_hour_capacity_for_team(num_sprint_days, team_member_details):
+    total_hours_available_per_team = 0
+    for member_detail in team_member_details:
+        total_hours_available_per_team += member_detail['hours_per_day'] * (num_sprint_days - sum(member_detail['time_off']))
+    return total_hours_available_per_team * len(team_member_details)
 
 
 prev_sprints_pt = [30, 85, 10, 25, 40]
@@ -20,5 +26,7 @@ team_mem_details = [
     {'hours_per_day': 6, 'time_off': [2, 0, 0]},  # Example details for team member 1
     {'hours_per_day': 5, 'time_off': [1, 0, 0]}   # Example details for team member 2
 ]
-print(calculate_effort_hour_capacity_per_person(number_sprint_days, team_mem_details))
-print(calculate_velocity(prev_sprints_pt))
+
+print(f'Velocity calculation: {calculate_velocity(prev_sprints_pt)}')
+print(f'Effort hour capacity per person : {calculate_effort_hour_capacity_per_person(number_sprint_days, team_mem_details)}')
+print(f'Effort hour capacity per person : {calculate_effort_hour_capacity_for_team(number_sprint_days, team_mem_details)}')
